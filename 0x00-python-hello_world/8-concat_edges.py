c90ba12640c49f9475daec3731e3bcb7acea75ee
Bayero-abdul <bayeroa65@gmail.com>
2022-06-27 17:04:25 +0000

    Debug code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 			  language that combines remarkable power with very clear syntax"
-<<<<<<< HEAD
 str = str[39:66] + " with " + str[:6]
-=======
-str = str[40:67] + " with " + str[:6]
->>>>>>> 76041055e57537b3ebc730075e7ecd65d48c2690
 print(str)
