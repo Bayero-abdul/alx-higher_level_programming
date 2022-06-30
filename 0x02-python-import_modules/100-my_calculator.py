7cfa67c7feff9b184f405e57e2dd6b182c8518f5
Bayero-abdul <bayeroa65@gmail.com>
2022-06-30 18:32:02 +0000

    Program that handles basic arithmetic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+
+    if len(argv) != 4:
+        print("usage: ./100-my_calculator.y <a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    else:
+        result = div(a, b)
+
+    print("{} {} {} = {}".format(a, op, b, result))
