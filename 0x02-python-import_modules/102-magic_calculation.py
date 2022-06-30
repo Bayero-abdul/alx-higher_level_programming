81ed8b11687a95ec6d1623ef05e53490b0c70fef
Bayero-abdul <bayeroa65@gmail.com>
2022-06-30 21:15:55 +0000

    Add function definition

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name == "__main__":
-    from magic_calculation_102 import add, sub
+from magic_calculation_102 import add, sub
+def def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6);
