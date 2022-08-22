89bcef0e90d4eea5c5682c138012731d9b977e72
Bayero-abdul <bayeroa65@gmail.com>
2022-08-22 15:54:13 +0000

    Check for NULL

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,19 +2,28 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/*
+* check_cycle - checks if there is a cycle in a singly linked list
+* @list: pointer to a singly linked list
+* Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+
 int check_cycle(listint_t *list)
 {
 	listint_t *hare = list;
 	listint_t *tortoise = list;
 
 	while (hare)
-	{	
-		hare = hare->next->next;
+	{
 		tortoise = tortoise->next;
-		
-		if (hare == tortoise)
-			return 1;
-	}
+		if (!tortoise)
+			return (0);
+		hare = hare->next->next;
+		if (!hare)
+			return (0);
 
-	return 0;
+		if (hare == tortoise)
+			return (1);
+	}
+	return (0);
 }
