951c85741759a58b4daf57a3e7f2b774c9e64170
Bayero-abdul <bayeroa65@gmail.com>
2022-08-25 08:30:36 +0000

    Remove 0x02-python-import_modules

--- 0x02-python-import_modules/0x02-python-import_modules/0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    """Prints and returns the value of the last digit"""
-    if number < 0:
-        number *= -1
-    print("{0:d}".format(number % 10), end="")
-    return (number % 10)
