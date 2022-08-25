4510c1bab739e061582afea6a6e1a178f53bc245
Bayero-abdul <bayeroa65@gmail.com>
2022-08-25 09:29:29 +0000

    Push 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from sys import exit, argv
+from calculator_1 import add, mul, sub, div
+
+if len(argv) < 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+
+a = int(argv[1])
+b = int(argv[3])
+sign = argv[2]
+
+if sign not in "+-*/":
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+if sign == "+":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+elif sign == "-":
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+elif sign == "*":
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+else:
+    print("{} / {} = {}".format(a, b, div(a, b)))
+
+exit(0)
