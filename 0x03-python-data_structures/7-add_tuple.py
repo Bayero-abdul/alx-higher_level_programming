6f39f81fd0853a68b60ef3dad292a5a587a3ec52
Bayero-abdul <bayeroa65@gmail.com>
2022-08-29 17:30:16 +0000

    Style code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,14 +8,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         first_elem = tuple_a[0]
         second_elem = tuple_a[1]
     elif len(tuple_b) < 2:
-        if tuple_b[0] == None:
+        if tuple_b[0] is None:
             first_elem = tuple_a[0]
             second_elem = tuple_a[1] + tuple_b[1]
         else:
             first_elem = tuple_a[0] + tuple_b[0]
-            second_elem = tuple_a[1] 
+            second_elem = tuple_a[1]
     else:
         first_elem = tuple_a[0] + tuple_b[0]
         second_elem = tuple_a[1] + tuple_b[1]
-    
-    return((first_elem, second_elem)) 
+
+    return((first_elem, second_elem))
