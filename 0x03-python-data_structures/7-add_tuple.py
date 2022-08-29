a33a301e820ca5450a7e41f6bf3009d0565a0662
Bayero-abdul <bayeroa65@gmail.com>
2022-08-29 18:26:03 +0000

    Test code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -13,6 +13,21 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif tuple_b == ():
         first_elem = tuple_a[0]
         second_elem = tuple_a[1]
+
+    elif len(tuple_a) < 2 and len(tuple_b) < 2:
+        if tuple_a[0] is None and tuple_b[0] is None:
+            first_elem = 0 + 0
+        elif tuple_a[0] is None and tuple_b[0] is not None:
+            first_elem = 0 + tuple_b[0]
+        else:
+            first_elem = tuple_a[0]
+
+        if tuple_a[1] is None and tuple_b[1] is None:
+            second_elem = 0
+        elif tuple_a[1] is None and tuple_b[1] is not None:
+            second_elem = tuple_b[1]
+        else:
+            second_elem = tuple_a[1]
     elif len(tuple_a) < 2:
         if tuple_a[0] is None:
             first_elem = tuple_b[0]
