9979d6ca03ff04479dc87bfb6655c1f4bd6f491d
Bayero-abdul <bayeroa65@gmail.com>
2022-08-31 16:13:00 +0000

    Add parenthesis

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or
-    roman_string is None or roman_string == "0":
+    if (not(isinstance(roman_string, str))) or roman_string is None:
         return (0)
 
     rom_to_int = {"I": 1, "V": 5, "X": 10, "L": 50,
