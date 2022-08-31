45ade01decd06d315f4d49c08a356d797e923d59
Bayero-abdul <bayeroa65@gmail.com>
2022-08-31 14:39:51 +0000

    Test code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None:
         return (None)
-    sorted_dict = sorted(a_dictionary.items(), key=lambda k: k[1])
-    best_student = sorted_dict[0][1]
+    else:
+        sorted_dict = sorted(a_dictionary.items(), key=lambda k: k[1])
+        best_student = sorted_dict[0][1]
 
-    return (best_student)
+        return (best_student)
