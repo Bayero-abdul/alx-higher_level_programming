891da0a186e3d980b8e43f935465dc48ba35c56a
Bayero-abdul <bayeroa65@gmail.com>
2022-08-31 16:10:27 +0000

    Test for "0"

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not isinstance(roman_string, str) or
+    roman_string is None or roman_string == "0":
         return (0)
 
     rom_to_int = {"I": 1, "V": 5, "X": 10, "L": 50,
