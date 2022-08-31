eacc8465fd342134098cd87d61abe595364938d7
Bayero-abdul <bayeroa65@gmail.com>
2022-08-31 21:43:04 +0000

    Use for loop

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,9 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
 
-    sorted_dict = sorted(a_dictionary.items(), key=lambda k: k[1])
-    best_student = sorted_dict[0][1]
+    max_num = 0
+    for key in a_dictionary:
+        if a_dictionary[key] >= max_num:
+            max_num = a_dictionary[key]
+            best_student = key
     return (best_student)
