8154b66f07410a0e52ccd1d99b85ee5bf80533c1
Bayero-abdul <bayeroa65@gmail.com>
2022-09-02 22:29:48 +0000

    Use three lines only

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if matrix == []:
-        return (None)
-
-    return(list(map(lambda x: square_list(x), matrix)))
-
-
-def square_list(my_list=[]):
-    return (list(map(lambda x: x**2, my_list)))
+    return(list(map(lambda x: list(map(lambda x: x**2, x)), matrix)))
