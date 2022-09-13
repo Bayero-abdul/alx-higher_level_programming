75d193bfd3c841246d8470bcedb130f177ac7911
Bayero-abdul <bayeroa65@gmail.com>
2022-09-13 20:18:04 +0000

    Correct Class doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
-    """Square is an empty class that does nothing"""
+    """Square is an empty class that defines a square.
+
+    Attributes:
+        None
+    
+    """
     pass
