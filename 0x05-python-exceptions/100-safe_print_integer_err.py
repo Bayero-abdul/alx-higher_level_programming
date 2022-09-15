1ce7965b76f2d09e5b0650bfdddb7db0c3f601f3
Bayero-abdul <bayeroa65@gmail.com>
2022-09-15 21:03:49 +0000

    Return False if value is not an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -9,3 +9,4 @@ def safe_print_integer_err(value):
     except ValueError:
         sys.stderr.write("Exception: Unknown format code 'd' for object of \
 type '{}'\n".format(value.__class__.__name__))
+        return False
