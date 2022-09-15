5e693ad82769c9947906e8e5c1c969ec42e10890
Bayero-abdul <bayeroa65@gmail.com>
2022-09-15 21:01:23 +0000

    Use try/except:

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    if isinstance(value, int):
+    try:
         print("{:d}".format(value))
         return True
-    else:
+    except ValueError:
         sys.stderr.write("Exception: Unknown format code 'd' for object of \
 type '{}'\n".format(value.__class__.__name__))
