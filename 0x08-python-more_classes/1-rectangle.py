fd36c242dd320857d14d88b6e69f8663118484b3
Bayero-abdul <bayeroa65@gmail.com>
2022-09-19 16:16:37 +0000

    Change setter's argument to value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,25 +11,25 @@ class Rectangle:
         self.height = height
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, width):
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >=0")
-        self.__width = width
-
-    @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        if type(height) != int:
+    def height(self, value):
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must >=0")
-        self.__height = height
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
