4558c2c77f8fe54e4dcd06e7e6d99023edf6b013
Bayero-abdul <bayeroa65@gmail.com>
2022-09-19 20:45:16 +0000

    Remove whitespace

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
         print("Bye rectangle...")
         if Rectangle.number_of_instances > 0:
             Rectangle.number_of_instances -= 1
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) != Rectangle:
