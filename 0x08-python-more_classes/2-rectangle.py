e58d1c8bcd167d540cfb3fdef5f76454f42ed30c
Bayero-abdul <bayeroa65@gmail.com>
2022-09-19 16:38:18 +0000

    Return 0 in perimeter() if width or height is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,4 +40,7 @@ class Rectangle:
 
     def perimeter(self):
         """Returns perimeter of a rectangle"""
-        return 2 * (self.width + self.height)
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
