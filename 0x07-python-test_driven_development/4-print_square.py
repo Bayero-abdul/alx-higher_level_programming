69f7f0c8142975e179651006f6236b742fbc607e
Bayero-abdul <bayeroa65@gmail.com>
2022-09-25 10:43:37 +0000

    Style code

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,15 +2,13 @@
 def print_square(size):
     """prints square."""
     if type(size) != int:
-        raise TypeError("size must be integer")
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise TypeError("size must be >= 0")
-    
-    if size == 0:
-        print("")
-    else:
-        for i in range(size):
-            for j in range(size):
-                print("#", end="")
-                if i != size - 1:
-                    print('\n')
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        end = ""
+        for j in range(size):
+            if j == size - 1:
+                end = '\n'
+            print("#", end=end)
