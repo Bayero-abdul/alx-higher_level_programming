fbaa45b00ac385c8703fc46f9485f586125ddd39
Bayero-abdul <bayeroa65@gmail.com>
2022-09-25 09:40:39 +0000

    Test when div is inf

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,11 +6,12 @@ Is_matrix_of_int_float()
 
 """
 
+
 def matrix_divided(matrix, div):
     """divides all elements of a matrix
 
     Returns:
-        new matrix of divided elements in the matrix
+    new matrix of divided elements in the matrix
     """
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
@@ -19,25 +20,24 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if not is_matrix_of_int_float(matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+                        of integers/floats")
 
     length = len(matrix[0])
-    if (sum([1 for l in matrix if len(l) != length]) >= 1):
+    if (sum([1 for lst in matrix if len(lst) != length]) >= 1):
         raise TypeError("Each row of the matrix must have the same size")
 
-    return [[round(num / div, 2) for num in l] for l in matrix]
+    return [[round(num / div, 2) for num in lst for lst in matrix]
 
 
 def is_matrix_of_int_float(matrix):
-    """Checks if a matrix is al ist of lists of integers or floats."""
-
     if type(matrix) != list or matrix == [] or matrix == [[]]:
         return 0
 
-    for l in matrix:
-        if type(l) != list:
+    for lst in matrix:
+        if type(lst) != list:
             return 0
-        for num in l:
+        for num in lst:
             if type(num) != int and type(num) != float:
                 return 0
     return 1
