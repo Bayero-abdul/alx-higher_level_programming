0cac5d7a173dad55bb1ba962e2021f8c7a00c42c
Bayero-abdul <bayeroa65@gmail.com>
2022-09-25 09:52:52 +0000

    Update code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """2-matrix_divided.py
-
 This modules contains a function, matrix_divided() and
 Is_matrix_of_int_float()
-
 """
 
 
 def matrix_divided(matrix, div):
     """divides all elements of a matrix
-
     Returns:
-    new matrix of divided elements in the matrix
+        new matrix of divided elements in the matrix
     """
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
@@ -21,16 +18,18 @@ def matrix_divided(matrix, div):
 
     if not is_matrix_of_int_float(matrix):
         raise TypeError("matrix must be a matrix (list of lists) \
-                        of integers/floats")
+of integers/floats")
 
     length = len(matrix[0])
     if (sum([1 for lst in matrix if len(lst) != length]) >= 1):
         raise TypeError("Each row of the matrix must have the same size")
 
-    return [[round(num / div, 2) for num in lst for lst in matrix]
+    return [[round(num / div, 2) for num in lst] for lst in matrix]
 
 
 def is_matrix_of_int_float(matrix):
+    """Checks if a matrix is al ist of lists of integers or floats."""
+
     if type(matrix) != list or matrix == [] or matrix == [[]]:
         return 0
 
