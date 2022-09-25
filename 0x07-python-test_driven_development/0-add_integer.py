43363786da3b543f4fd03b78cba649bb12d68116
Bayero-abdul <bayeroa65@gmail.com>
2022-09-25 07:09:38 +0000

    Check for inf and Nan values

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,16 @@ def add_integer(a, b=98):
     Returns:
         the sum of a and b
     """
+    if isinstance(a, float) and a != a:
+        a = 89
+    if isinstance(b, float) and b != b:
+        b = 89
     if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
-
     if type(b) != float and type(b) != int:
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
     return a + b
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return int(a) + int(b)
