76da6ce261679334ae20c2f047f68d255262788f
Bayero-abdul <bayeroa65@gmail.com>
2022-09-28 06:52:06 +0000

    Document code

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module contain a lookup() function."""
+
+
 def lookup(obj):
+    """returns the list of available attributes and methods of an object."""
     if isinstance(obj, object):
         return dir(obj)
