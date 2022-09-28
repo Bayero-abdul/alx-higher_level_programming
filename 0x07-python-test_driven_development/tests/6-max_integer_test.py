80687f9661935d7d0a8bf555933c42d4a7f1c907
Bayero-abdul <bayeroa65@gmail.com>
2022-09-28 20:29:47 +0000

    Test for max integer in a list

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Tests class for testing Maximum integer in a list."""
+
+    def test_list_ints(self):
+        self.assertEqual(max_integer([3, -1, 9]), 9)
+        self.assertEqual(max_integer([-1, -5, -4]), -1)
+        self.assertEqual(max_integer([9]), 9)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_typeerorrs(self):
+        with self.assertRaises(TypeError):
+            max_integer(4)
+
+        with self.assertRaises(TypeError):
+            max_integer([4, "43", 6])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
