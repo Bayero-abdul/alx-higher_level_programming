9a59e882c58c17b0a979a230e879018d0407e93b
Bayero-abdul <bayeroa65@gmail.com>
2022-09-30 15:09:10 +0000

    Change name of instance variable

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,15 +8,15 @@ class MagicClass:
     """Defines MagicClass objects"""
     def __init__(self, radius=0):
         """Initializes values of instances"""
-        self._MagicClass__radius = radius
+        self.__radius = radius
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        return self._MagicClass__radius
+        return self.__radius
 
     def area(self):
         """returns the area of a circle"""
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """returns the circumference of a circle"""
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius
