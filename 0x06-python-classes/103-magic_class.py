1105298ad487039086ced1bc0329a0f3b3614b94
Bayero-abdul <bayeroa65@gmail.com>
2022-09-30 14:36:29 +0000

    Write the Python class MagicClass that does some magic

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module contains class MagicClass and
+area() and circumference()"""
+import math
+
+
+class MagicClass:
+    """Defines MagicClass objects"""
+    def __init__(self, radius):
+        """Initializes values of instances"""
+        self._MagicClass__radius = radius
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        return self.magicClass
+
+    def area(self):
+        """returns the area of a circle"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """returns the circumference of a circle"""
+        return 2 * math.pi * self._MagicClass__radius
