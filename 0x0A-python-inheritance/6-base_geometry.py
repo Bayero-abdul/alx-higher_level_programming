2eae7d184d3549ac5fc3bbb3d5edfdd7f31c71ea
Bayero-abdul <bayeroa65@gmail.com>
2022-10-02 13:51:30 +0000

    Add public method area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module contains ``class BaseGeometry``"""
+
+
+class BaseGeometry:
+    """Defines BaseGeometry"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
