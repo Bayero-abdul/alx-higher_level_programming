2c06cc292bd97aef17d1f6511e07531d45c8b39e
Bayero-abdul <bayeroa65@gmail.com>
2022-10-05 10:30:37 +0000

    Refactor code

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,26 +5,16 @@
 def pascal_triangle(n):
     """returns a list of lists of integers representing the \
     Pascals triangle of n"""
-    pascal_lst = []
-    for i in range(n):
-        if i == 0:
-            pascal_lst.append([1])
-            continue
-        lst = []
-        lst_len = i + 1
-        for j in range(lst_len):
-            prev_lst = pascal_lst[i - 1]
-            if j == 0:
-                lst.append(1)
-                continue
-            if j == 1 and len(prev_lst) == 1:
-                lst.append(1)
-                break
 
-            if j >= ((lst_len // 2) + 1):
-                lst.append(lst[abs(j - (lst_len - 1))])
-                continue
-            if j < len(prev_lst):
-                lst.append(prev_lst[j - 1] + prev_lst[j])
+    if n <= 0:
+        return []
+
+    pascal_lst = [[1]]
+    while len(pascal_lst) != n:
+        prev_lst = pascal_lst[-1]
+        lst = [1]
+        for i in range(len(prev_lst) - 1):
+            lst.append(prev_lst[i] + prev_lst[i + 1])
+        lst.append(1)
         pascal_lst.append(lst)
     return pascal_lst
