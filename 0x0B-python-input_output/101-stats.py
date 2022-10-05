19afe6a46ae0261e98fd99b666328cbfbc41ea99
Bayero-abdul <bayeroa65@gmail.com>
2022-10-05 18:28:36 +0000

    Write a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,25 +2,22 @@
 """Module contain reads stdin line by line and computes metric"""
 
 
-import os
+import sys
 
-status_code = [200, 301, 400, 401, 403, 404, 405, 500]
+status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
 metrics = {}
 total_size = 0
 line_cnt = 0
-read_fd = os.pipe()
-read_file = os.fdopen()
-while line in read_file:
+for line in sys.stdin:
     if line_cnt == 10:
         print(f"File size: {total_size}")
-        for key, value in metrics.key():
+        for key, value in sorted(metrics.items()):
             print(f"{key}: {value}")
         metrics.clear()
         line_cnt = 0
-    
-    status = line.split()[2]
+
+    status = line.split()[-2]
     if status in status_code:
-        total_size += line.split()[-1]
+        total_size += int(line.split()[-1])
         metrics[status] = metrics.get(status, 0) + 1
-        line_cnt += 1
-os.close(read_fd)
+    line_cnt += 1
