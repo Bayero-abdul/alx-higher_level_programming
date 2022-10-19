63fb6680e057dc03d2f20a192d129e67f4d263fa
Bayero-abdul <bayeroa65@gmail.com>
2022-10-19 11:04:17 +0000

    Use try and except blocks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,28 +2,33 @@
 """Module contain reads stdin line by line and computes metric"""
 
 
-import sys, signal
+import sys
 
-def handler(signal, frame):
-    print(f"File size: {total_size}")
-    for key, value in sorted(metrics.items()):
-        print(f"{key}: {value}")
-
-signal.signal(signal.SIGINT, handler)
 status_code = ["200", "301", "400", "401", "403", "404", "405", "500"]
 metrics = {}
 total_size = 0
 line_cnt = 0
-for line in sys.stdin:
-    if line_cnt == 10:
-        print(f"File size: {total_size}")
-        for key, value in sorted(metrics.items()):
-            print(f"{key}: {value}")
-        metrics.clear()
-        line_cnt = 0
 
-    status = line.split()[-2]
-    if status in status_code:
-        total_size += int(line.split()[-1])
-        metrics[status] = metrics.get(status, 0) + 1
-    line_cnt += 1
+try:
+    for line in sys.stdin:
+        if line_cnt == 10:
+            print(f"File size: {total_size}")
+            for key, value in sorted(metrics.items()):
+                print(f"{key}: {value}")
+            line_cnt = 1
+
+        try:
+            status = line.split()[-2]
+            if status in status_code:
+                total_size += int(line.split()[-1])
+                metrics[status] = metrics.get(status, 0) + 1
+        except Exception:
+            pass
+
+        line_cnt += 1
+
+except KeyboardInterrupt:
+    print(f"File size: {total_size}")
+    for key, value in sorted(metrics.items()):
+        print(f"{key}: {value}")
+    raise
