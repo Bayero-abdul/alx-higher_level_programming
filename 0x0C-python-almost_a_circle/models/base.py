baf850e26350f05c444c48a3e2be76beb828a6a3
Bayero-abdul <bayeroa65@gmail.com>
2022-11-24 03:09:31 +0000

    Use Base instead of type(self) in initializing attrs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,8 @@ class Base:
         if type(id) is int:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            Base.__nb_objects += 1
+            Base.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
