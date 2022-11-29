f3afc4c17d491f995ad2a29737fb2c4a16dbc3aa
Bayero-abdul <bayeroa65@gmail.com>
2022-11-29 01:26:05 +0000

    Update code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,9 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const numbers = [];
-  const args = process.argv.length;
+  const args = process.argv;
   for (let i = 2; i < args.length; i++) {
-    numbers.push(Number(process.argv[i]));
+    numbers.push(Number(args[i]));
   }
   numbers.sort();
   console.log(numbers[numbers.length - 2]);
