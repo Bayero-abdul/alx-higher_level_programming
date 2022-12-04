012dc516f790ab48ecfd3306a461bb68d006cb7c
Bayero-abdul <bayeroa65@gmail.com>
2022-12-04 18:30:59 +0000

    Update code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     rows = cur.fetchall()
 
     for row in rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
 
     cur.close()
     db.close()
