a22195a8d1855eb6cd3a4ead39c072bb24913c20
Bayero-abdul <bayeroa65@gmail.com>
2022-12-16 09:12:51 +0000

    Use the -s option

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -I 0.0.0.0:5000 | grep Content-Length |  cut -d " " -f 2
+curl -s -I 0.0.0.0:5000 | grep Content-Length |  cut -d " " -f 2
