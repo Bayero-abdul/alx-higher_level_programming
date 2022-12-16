448c1051042176ef9de5422723caa2c4ba0b2281
Bayero-abdul <bayeroa65@gmail.com>
2022-12-16 09:39:02 +0000

    Update file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of the body of the response
-echo `curl -s -I 0.0.0.0:5000 | grep Content-Length |  cut -d " " -f 2`
+curl -s -I $1 | grep Content-Length |  cut -d " " -f 2
