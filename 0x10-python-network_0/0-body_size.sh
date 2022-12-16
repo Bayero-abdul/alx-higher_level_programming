3c709aead825ab05010176c8161940695a384c20
Bayero-abdul <bayeroa65@gmail.com>
2022-12-16 09:31:09 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -I 0.0.0.0:5000 | grep Content-Length |  cut -d " " -f 2
+# displays the size of the body of the response
+echo `curl -s -I 0.0.0.0:5000 | grep Content-Length |  cut -d " " -f 2`
