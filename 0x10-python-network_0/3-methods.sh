4ea0d72237a04e9a085b111251a50770c5938d63
Bayero-abdul <bayeroa65@gmail.com>
2022-12-19 14:02:45 +0000

    Complete task

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -sI $1 | grep Allow |  cut -d " " -f 2-4
+curl -sI 0.0.0.0:5000/route_4 | grep Allow | sed -r 's/\S*[[:space:]](.*)/\1/'
