ab2fd0e2d3c06aa6267f338fd22c9d69b1d7d1ed
Bayero-abdul <bayeroa65@gmail.com>
2022-12-19 10:29:52 +0000

    Test task 1

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# displays the size of the body of the response
+curl -s 0.0.0.0:5000/route_1 | grep route_2 | sed 's/.*\(route_2\).*/\1/' | sed -r 's/([a-z]+).([0-9]+)/\1\2/' | sed 's/./\U&/'
