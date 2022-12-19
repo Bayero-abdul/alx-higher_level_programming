ae653a85fcc6791ffd183d048ecde546f47bb152
Bayero-abdul <bayeroa65@gmail.com>
2022-12-19 16:41:23 +0000

    Specigy the Content-type

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sX POST $1 -d @$1
+curl -H 'Content-Type: application/json' -sX POST $1 -d @$1
