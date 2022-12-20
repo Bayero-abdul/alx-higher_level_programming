fe1bbc51c6b2fe3a3392af00a5a337bd5a160af8
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 10:44:52 +0000

    Send a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response using the requests module.
+
+"""
+
+
+def main():
+    from sys import argv
+    import requests
+
+    url, email = argv[1], argv[2]
+    r = requests.post(url, data={'email': email})
+    print(r.text)
+
+if __name__ == '__main__':
+    main()
