4c94de99cb33030965cc8fc8905823305929fd9c
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 08:44:47 +0000

    Sends a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response.
+
+"""
+
+def main():
+    from sys import argv
+    from urllib import request
+    from urllib import parse
+
+    url, email = argv[1], argv[2]
+    data = parse.urlencode({'email': 'hr@holbertonschool.com'})
+    data = data.encode('ascii')
+    with request.urlopen(url, data) as f:
+        print(f.read().decode('utf-8'))
+
+if __name__ == '__main__':
+    main()
