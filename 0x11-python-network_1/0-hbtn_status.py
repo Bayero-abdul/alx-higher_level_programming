9547ec9b4e6c0efa08340b1dbb8fe60321544db5
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 10:05:29 +0000

    Format output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status."""
 
-import urllib.request
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print(f'Body response:\n\t\
+def main():
+    import urllib.request
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print(f'Body response:\n\t\
 - type: {type(body)}\n\t\
 - content: {body}\n\t\
 - utf8 content: {body.decode("utf-8")}')
+
+
+if __name__ == '__main__':
+    main()
