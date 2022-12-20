fe8b434ea17a68a4f1c6f3be8824b44b59162815
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 09:43:06 +0000

    Correct output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,13 +7,13 @@ def main():
     from urllib.request import urlopen
     from urllib.error import URLError, HTTPError
 
-
     url = argv[1]
     try:
         with urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as e:
         print('Error code:', e.code)
+
 
 if __name__ == '__main__':
     main()
