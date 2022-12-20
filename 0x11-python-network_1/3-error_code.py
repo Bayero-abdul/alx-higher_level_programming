3cc348b8e3ef843aee12432d0a231ab329b637ba
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 09:21:46 +0000

    Add with keyword

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,17 +4,16 @@
 
 def main():
     from sys import argv
-    from urllib.request import Request, urlopen
+    from urllib.request import urlopen
     from urllib.error import URLError, HTTPError
 
+
     url = argv[1]
-    req = Request(url)
     try:
-        response = urlopen(req)
+        with urlopen(url) as reponse:
+            print(response.read().decode('utf-8'))
     except HTTPError as e:
         print('Error code: ', e.code)
-    else:
-        print(response.read().decode('utf-8'))
 
 
 if __name__ == '__main__':
