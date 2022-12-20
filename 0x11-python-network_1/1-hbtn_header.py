ed28d0fe33282f052dfa9535ee82d435f7e77649
Bayero-abdul <bayeroa65@gmail.com>
2022-12-20 08:08:19 +0000

    Make code non-executable when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,9 +4,15 @@ of the X-Request-Id variable found in the header of the response.
 
 """
 
-import sys
-import urllib.request
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers['X-Request-Id'])
+def main():
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    main()
