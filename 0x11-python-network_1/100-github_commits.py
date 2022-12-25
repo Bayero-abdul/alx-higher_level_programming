ee008f108ba69b3fe4fdf89e867169d54e1bb7b4
Bayero-abdul <bayeroa65@gmail.com>
2022-12-25 05:57:43 +0000

    Test code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,33 +4,19 @@ repository "rails" by the user “rails”
 
 """
 
-"""
+
 def main():
-    from sys import argv
+    import sys
     import requests
 
-    user, repo = argv[1], argv[2]
+    user, repo = sys.argv[1], sys.argv[2]
     r = requests.get(f'https://api.github.com/repos/{user}/{repo}/commits')
-    commits = r.json()
-    commit_list = commits[:10]
-    
-    for commit in commit_list:
+    commits = r.json()[:10]
+
+    for commit in commits:
         print('{}: {}'.format(commit.get('sha'),
               commit.get('commit').get('author').get('name')))
 
 
 if __name__ == '__main__':
     main()
-"""
-
-if __name__ == '__main__':
-    import sys
-    import requests
-
-    a = sys.argv[1:]
-    coms = requests.get(f"https://api.github.com/repos/{a[1]}/{a[0]}/commits")
-    top_ten = coms.json()[:10]
-
-    for i in top_ten:
-        print(f"{i.get('sha')}:", end=" ")
-        print(i.get('commit').get('author').get('name'))
