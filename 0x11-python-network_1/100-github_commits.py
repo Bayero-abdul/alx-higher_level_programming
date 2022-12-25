09c80903ca14e043b48389cbf2f91bd4a6cf6e0c
Bayero-abdul <bayeroa65@gmail.com>
2022-12-25 05:41:06 +0000

    Test code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,8 +11,9 @@ def main():
 
     user, repo = argv[1], argv[2]
     r = requests.get(f'https://api.github.com/repos/{user}/{repo}/commits')
-    commit_list = r.json()[:10]
-
+    commits = r.json()
+    commit_list = commits[:10]
+    
     for commit in commit_list:
         print('{}: {}'.format(commit.get('sha'),
               commit.get('commit').get('author').get('name')))
