e8bfd5c5b1f0f1b50ef5b3a6fecc6c698256c7a9
Bayero-abdul <bayeroa65@gmail.com>
2023-01-17 13:33:45 +0000

    Add README

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,19 +5,14 @@ repository "rails" by the user “rails”
 """
 
 
-def main():
-    from sys import argv
+if __name__ == '__main__':
+    import sys
     import requests
 
-    user, repo = argv[1], argv[2]
+    user, repo = sys.argv[1], sys.argv[2]
     r = requests.get(f'https://api.github.com/repos/{user}/{repo}/commits')
-    commits = r.json()
-    commit_list = commits[:10]
-    
-    for commit in commit_list:
-        print('{}: {}'.format(commit.get('sha'),
-              commit.get('commit').get('author').get('name')))
+    author_commit = r.json()[:10]
 
-
-if __name__ == '__main__':
-    main()
+    for commit in author_commit:
+        print(f"{commit.get('sha')}:", end=" ")
+        print(commit.get('commit').get('author').get('name'))
