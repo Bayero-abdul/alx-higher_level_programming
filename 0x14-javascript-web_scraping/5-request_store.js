f5966873a0949b2fc2203f8c0bf9eb4b7e1676ca
Bayero-abdul <bayeroa65@gmail.com>
2023-01-18 21:02:22 +0000

    Style code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,16 +5,15 @@ const request = require('request');
 const URL = process.argv[2];
 const PATH = process.argv[3];
 
-
 request(URL, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const data = new Uint8Array(Buffer.from(body));
-    fs.writeFile(PATH, data, {encoding: 'utf8'}, (err) => {
+    fs.writeFile(PATH, data, { encoding: 'utf8' }, (err) => {
       if (err) {
         console.log(err);
-       }
+      }
     });
   }
 });
