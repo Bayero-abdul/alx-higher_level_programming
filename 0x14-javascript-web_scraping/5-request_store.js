40dd054c42ece56004864a418f2721e25ddde3ba
Bayero-abdul <bayeroa65@gmail.com>
2023-01-26 00:57:11 +0000

    Complete task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,24 @@
 #!/usr/bin/node
+// send a request to star wars api
 
-const fs = require('fs');
 const request = require('request');
-const URL = process.argv[2];
-const PATH = process.argv[3];
-
-request(URL, (error, response, body) => {
+const url = process.argv[2];
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    const data = new Uint8Array(Buffer.from(body));
-    fs.writeFile(PATH, data, { encoding: 'utf8' }, (err) => {
-      if (err) {
-        console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body);
+
+    let sum = 0;
+    let film;
+    let char;
+    for (film in films.results) {
+      for (char in films.results[film].characters) {
+        if (films.results[film].characters[char].includes('18')) {
+          sum++;
+        }
       }
-    });
+    }
+    console.log(sum);
   }
 });
