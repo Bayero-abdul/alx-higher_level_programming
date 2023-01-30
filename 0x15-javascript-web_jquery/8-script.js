84b1eaa0a752e10a0cf9eab72cc81be54ea78a84
root <root@DESKTOP-7EFHCPC.localdomain>
2023-01-30 22:09:28 +0000

    Push tasks

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$.ajax({
+  method: "GET",
+  url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+  success: function (data) {
+    $('UL#list_movies').append(data.results.map(movie => `<li>${movie.title}</li>`));
+  }
+});
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$(function () {
+    $.ajax({
+      url: "https://fourtonfish.com/hellosalut/?lang=fr",
+      success: function (data) {
+        $('DIV#hello').text(data.hello);
+      }
+    });
+});
